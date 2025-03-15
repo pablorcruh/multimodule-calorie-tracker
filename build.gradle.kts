3d45bed9a36d17af6e1067b64ac339272cca2829
pablorcruh <pablorcruh@gmail.com>
2025-03-14 21:15:31 -0500

    up to navigation util

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ buildscript {
         classpath(Build.androidBuildTools)
         classpath(Build.kotlinGradlePlugin)
         classpath(Build.hiltAndroidGradlePlugin)
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
