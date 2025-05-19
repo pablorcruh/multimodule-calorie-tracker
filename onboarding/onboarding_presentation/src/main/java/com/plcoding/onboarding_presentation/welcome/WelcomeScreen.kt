2c872ff0c56e797845a5fdb81d808e163a19b5c4
pablorcruh <pablorcruh@gmail.com>
2025-05-18 21:19:43 -0500

    up to gender screen

--- onboarding/onboarding_presentation/src/main/java/com/plcoding/onboarding_presentation/welcome/WelcomeScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/com/plcoding/onboarding_presentation/welcome/WelcomeScreen.kt
@@ -40,7 +40,7 @@ fun WelcomeScreen(
         Spacer(modifier = Modifier.height(spacing.spaceMedium))
         ActionButton(
             text = stringResource(id = com.plcoding.core.R.string.next),
-            onClick = { onNavigate(UiEvent.Navigate(Route.AGE))},
+            onClick = { onNavigate(UiEvent.Navigate(Route.GENDER))},
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
     }
