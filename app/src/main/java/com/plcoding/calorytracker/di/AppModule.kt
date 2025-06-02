9eb068abce23f4ffe481bb732ccdfb634451e029
pablorcruh <pablorcruh@gmail.com>
2025-06-01 21:09:42 -0500

    up to age screen

--- app/src/main/java/com/plcoding/calorytracker/di/AppModule.kt
+++ app/src/main/java/com/plcoding/calorytracker/di/AppModule.kt
@@ -5,6 +5,7 @@ import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
 import com.plcoding.core.domain.DefaultPreferences
 import com.plcoding.core.domain.preferences.Preferences
+import com.plcoding.core.domain.usecase.FilterOutDigits
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -27,6 +28,12 @@ object AppModule {
     @Singleton
     fun providePreferences(sharedPreferences: SharedPreferences): Preferences {
         return DefaultPreferences(sharedPreferences)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFilterOutDigitsUseCase(): FilterOutDigits{
+        return FilterOutDigits()
     }
 
 }
