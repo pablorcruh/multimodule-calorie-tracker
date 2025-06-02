96299f944a1c99244ca178a0a900225cfbea27c7
pablorcruh <pablorcruh@gmail.com>
2025-06-01 21:52:15 -0500

    up to height and weight screen

--- onboarding/onboarding_presentation/src/main/java/com/plcoding/onboarding_presentation/height/HeightScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/com/plcoding/onboarding_presentation/height/HeightScreen.kt
@@ -1,4 +1,4 @@
-package com.plcoding.onboarding_presentation.age
+package com.plcoding.onboarding_presentation.height
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -24,12 +24,12 @@ import com.plcoding.onboarding_presentation.components.ActionButton
 import com.plcoding.onboarding_presentation.components.UnitTextField
 
 @Composable
-fun AgeScreen(
+fun HeightScreen(
     scaffoldState: ScaffoldState,
     onNavigate: (UiEvent.Navigate) -> Unit,
-    viewModel: AgeViewModel = hiltViewModel()
-
+    viewModel: HeightViewModel = hiltViewModel()
 ){
+
     val spacing = LocalSpacing.current
     val context = LocalContext.current
 
@@ -58,14 +58,14 @@ fun AgeScreen(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-                text = stringResource(id = R.string.whats_your_age),
+                text = stringResource(id = R.string.whats_your_height),
                 style = MaterialTheme.typography.h3
             )
             Spacer(modifier = Modifier.height(spacing.spaceMedium))
             UnitTextField(
-                value = viewModel.age,
-                onValueChange = viewModel::onAgeEnter,
-                unit = stringResource(id = R.string.years)
+                value = viewModel.height,
+                onValueChange = viewModel::onHeightEnter,
+                unit = stringResource(id = R.string.cm)
             )
         }
         ActionButton(
@@ -74,4 +74,5 @@ fun AgeScreen(
             modifier = Modifier.align(Alignment.BottomEnd)
         )
     }
+
 }
