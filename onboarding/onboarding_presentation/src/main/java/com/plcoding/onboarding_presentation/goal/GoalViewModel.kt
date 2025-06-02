5bf5eed2fbaeec3d71189b41fbf127cfc2561a97
pablorcruh <pablorcruh@gmail.com>
2025-06-01 22:34:02 -0500

    up to activity and goal screen

---
+++ onboarding/onboarding_presentation/src/main/java/com/plcoding/onboarding_presentation/goal/GoalViewModel.kt
@@ -0,0 +1,39 @@
+package com.plcoding.onboarding_presentation.goal
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.plcoding.core.domain.model.GoalType
+import com.plcoding.core.domain.preferences.Preferences
+import com.plcoding.core.navigation.Route
+import com.plcoding.core.util.UiEvent
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class GoalViewModel @Inject constructor(
+    private val preferences: Preferences
+): ViewModel(){
+
+    var selectedGoalType by mutableStateOf<GoalType>(GoalType.KeepWeight)
+        private set
+
+    private val _uiEvent = Channel<UiEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    fun onGoalTypeSelected(goalType: GoalType){
+        selectedGoalType = goalType
+    }
+
+    fun onNextClick(){
+        viewModelScope.launch {
+            preferences.saveGoalType(selectedGoalType)
+            _uiEvent.send(UiEvent.Navigate(Route.NUTRIENT_GOAL))
+        }
+    }
+}
